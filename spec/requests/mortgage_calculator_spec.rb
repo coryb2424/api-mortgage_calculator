598d643f295628a0275fe31d63fd7038bb5908d2
cory bonneau <cory.bonneau2424@gmail.com>
2018-10-21 09:35:49 -0700

    added routes and test for mortgage amount

--- spec/requests/mortgage_calculator_spec.rb
+++ spec/requests/mortgage_calculator_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Mortgage Calculator', type: :request do
-  context 'when the request is valid' do
+  context 'GET /payment-amount' do
     let(:valid_params) do
       {
         asking_price: '750000',
@@ -11,7 +11,16 @@ RSpec.describe 'Mortgage Calculator', type: :request do
       }
     end
 
-    describe 'GET /payment-amount' do
+    let(:invalid_params_down_payment) do
+      {
+        asking_price: '750000',
+        down_payment: '10000',
+        payment_schedule: 'biweekly',
+        amortization_period: '10'
+      }
+    end
+
+    describe 'when the request is valid' do
       before { get '/payment-amount', params: valid_params }
 
       it 'returns correct amount' do
@@ -22,20 +31,54 @@ RSpec.describe 'Mortgage Calculator', type: :request do
         expect(response).to have_http_status(200)
       end
     end
+
+    describe 'when the request is invalid due to low down payment' do
+      before { get '/payment-amount', params: invalid_params_down_payment }
+
+      it 'returns validation error message' do
+        expect(response.body)
+          .to match(/Validation failed/)
+      end
+
+      it 'returns status code 422' do
+        expect(response).to have_http_status(422)
+      end
+    end
   end
 
-  context 'when the request is invalid due to low down payment' do
-    let(:invalid_params_down_payment) do
+  context 'GET /mortgage-amount' do
+    let(:valid_params) do
       {
-        asking_price: '750000',
-        down_payment: '10000',
-        payment_schedule: 'biweekly',
+        payment_amount: '1500',
+        down_payment: '50000',
+        payment_schedule: 'monthly',
         amortization_period: '10'
       }
     end
 
-    describe 'GET /payment-amount' do
-      before { get '/payment-amount', params: invalid_params_down_payment }
+    let(:invalid_params_amort_period) do
+      {
+        payment_amount: '1500',
+        down_payment: '50000',
+        payment_schedule: 'monthly',
+        amortization_period: '3'
+      }
+    end
+
+    describe 'when the request is valid' do
+      before { get '/mortgage-amount', params: valid_params }
+
+      it 'returns correct amount' do
+        expect(json['amount']).to eq(209117.59)
+      end
+
+      it 'returns status code 200' do
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    describe 'when the request is invalid due to low down payment' do
+      before { get '/mortgage-amount', params: invalid_params_amort_period }
 
       it 'returns validation error message' do
         expect(response.body)
